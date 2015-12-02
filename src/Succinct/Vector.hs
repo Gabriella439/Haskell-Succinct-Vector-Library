b585729508f5b54636781f5e47ffeb57c78ac1ba
Gabriel Gonzalez <Gabriel439@gmail.com>
2015-12-01 19:17:25 -0800

    First draft of code to build `select9` index

--- src/Succinct/Vector.hs
+++ src/Succinct/Vector.hs
@@ -30,12 +30,17 @@ module Succinct.Vector (
     , unsafeRank
     ) where
 
+-- TODO: Use `Unboxed` vectors instead of `Primitive`?
+-- TODO: Compress original bit vector
+
 import Control.DeepSeq (NFData(..))
 import Data.Bits ((.|.), (.&.))
-import Data.Word (Word64)
+import Data.Word (Word16, Word64)
 
 import qualified Data.Bits             as Bits
+import qualified Data.Vector.Generic   as Generic
 import qualified Data.Vector.Primitive as Primitive
+import qualified Data.Vector.Unboxed   as Unboxed
 
 -- $setup
 -- >>> :set -XScopedTypeVariables
@@ -67,16 +72,23 @@ index i n =
     The `SuccinctBitVector` increases the original bit vector's size by 25%
 -}
 data SuccinctBitVector = SuccinctBitVector
-    { size  :: {-# UNPACK #-} !Int
+    { size    :: !Int
     -- ^ Size of original bit vector, in bits
-    , rank9 :: {-# UNPACK #-} !(Primitive.Vector Word64)
+    , rank9   :: !(Primitive.Vector Word64)
     -- ^ Two-level index of cached rank calculations at Word64 boundaries
-    , bits  :: {-# UNPACK #-} !(Primitive.Vector Word64)
+    , select9 :: !Select9
+    -- ^ Primary and secondary inventory used for select calculations
+    , bits    :: !(Primitive.Vector Word64)
     -- ^ Original bit vector
-    }
+    } deriving (Show)
 
 instance NFData SuccinctBitVector where
     rnf i = i `seq` ()
+
+data Select9 = Select9
+    { primary   :: Unboxed.Vector Int
+    , secondary :: Unboxed.Vector Word64
+    } deriving (Show)
 
 data Level = First | Second
 
@@ -99,58 +111,173 @@ popCount x0 = Bits.unsafeShiftR (x3 * 0x0101010101010101) 56
 -}
 prepare :: Primitive.Vector Word64 -> SuccinctBitVector
 prepare v = SuccinctBitVector
-    { size  = len * 64
-    , rank9 = Primitive.unfoldrN iLen iStep iBegin
-    , bits  = v
+    { size    = lengthInBits
+    , rank9   = vRank
+    , select9 = Select9 v1 v2
+    , bits    = v
     }
   where
+    lengthInBits = len * 64
+
     len = Primitive.length v
 
-    iLen = 2 * (((len - 1) `div` 8) + 1) + 1
+    -- TODO: What happens if `len == 0`?
+    vRankLen = 2 * (((len - 1) `div` 8) + 1) + 1
 
-    iStep (Status level r i0) = Just (case level of
-        First  -> (r , Status Second r       i0)
-        Second -> (r', Status First (r + r8) i8) )
-          where
-            i1 = i0 + 1
-            i2 = i1 + 1
-            i3 = i2 + 1
-            i4 = i3 + 1
-            i5 = i4 + 1
-            i6 = i5 + 1
-            i7 = i6 + 1
-            i8 = i7 + 1
+    vRank = Primitive.unfoldrN vRankLen iStep iBegin
+      where
+        iStep (Status level r i0) = Just (case level of
+            First  -> (r , Status Second r       i0)
+            Second -> (r', Status First (r + r8) i8) )
+              where
+                i1 = i0 + 1
+                i2 = i1 + 1
+                i3 = i2 + 1
+                i4 = i3 + 1
+                i5 = i4 + 1
+                i6 = i5 + 1
+                i7 = i6 + 1
+                i8 = i7 + 1
 
-            count i =
-                if i < len
-                then popCount (Primitive.unsafeIndex v i)
-                else 0
+                count i =
+                    if i < len
+                    then popCount (Primitive.unsafeIndex v i)
+                    else 0
 
-            r1 =      count i0
-            r2 = r1 + count i1
-            r3 = r2 + count i2
-            r4 = r3 + count i3
-            r5 = r4 + count i4
-            r6 = r5 + count i5
-            r7 = r6 + count i6
-            r8 = r7 + count i7
+                r1 =      count i0
+                r2 = r1 + count i1
+                r3 = r2 + count i2
+                r4 = r3 + count i3
+                r5 = r4 + count i4
+                r6 = r5 + count i5
+                r7 = r6 + count i6
+                r8 = r7 + count i7
 
-            r' = r1
-             .|. Bits.unsafeShiftL r2  9
-             .|. Bits.unsafeShiftL r3 18
-             .|. Bits.unsafeShiftL r4 27
-             .|. Bits.unsafeShiftL r5 36
-             .|. Bits.unsafeShiftL r6 45
-             .|. Bits.unsafeShiftL r7 54
+                r' = r1
+                 .|. Bits.unsafeShiftL r2  9
+                 .|. Bits.unsafeShiftL r3 18
+                 .|. Bits.unsafeShiftL r4 27
+                 .|. Bits.unsafeShiftL r5 36
+                 .|. Bits.unsafeShiftL r6 45
+                 .|. Bits.unsafeShiftL r7 54
 
-    iBegin = Status First 0 0
+        iBegin = Status First 0 0
+
+    bitIndices :: Unboxed.Vector Int
+    bitIndices =
+        ( Unboxed.map (\(i, _) -> i)
+        . Unboxed.filter (\(_, b) -> b)
+        . Unboxed.imap (,)
+        . Unboxed.concatMap (\w64 ->
+            Unboxed.generate 64 (Bits.testBit w64) )
+        . Generic.convert
+        ) v
+
+    -- TODO: Check to see if point-free style interferes with fusion
+    v1 :: Unboxed.Vector Int
+    v1 =
+        ( flip Unboxed.snoc lengthInBits
+        . Unboxed.map (\(_, i) -> i)
+        . Unboxed.filter (\(j, _) -> j `rem` 512 == 0)
+        . Unboxed.imap (,)
+        . oneIndices
+        ) v
+
+    oneIndices :: Primitive.Vector Word64 -> Unboxed.Vector Int
+    oneIndices =
+          Unboxed.map (\(i, _) -> i)
+        . Unboxed.filter (\(_, b) -> b)
+        . Unboxed.imap (,)
+        . Unboxed.concatMap (\w64 ->
+            Unboxed.generate 64 (Bits.testBit w64) )
+        . Generic.convert
+
+    count :: Int -> Word64
+    count basicBlockIndex =
+        let i = basicBlockIndex * 2
+        in  if i < Primitive.length vRank
+            then Primitive.unsafeIndex vRank i
+            else fromIntegral (maxBound :: Word16)
+
+    -- TODO: What if the vector is empty?
+    locate :: Unboxed.Vector Int -> Int -> Int
+    locate v i =
+        if i < Unboxed.length v
+        then Unboxed.unsafeIndex v i
+        else 0
+
+    v2 =
+        ( Unboxed.concatMap (\(p, q) ->
+            -- TODO: Explain the deviation from the paper here
+            let basicBlockBegin = p `div` 512
+                basicBlockEnd   = q `div` 512
+                numBasicBlocks  = basicBlockEnd - basicBlockBegin
+                span            = numBasicBlocks * 2
+            in  case () of
+                  _ | numBasicBlocks < 1 ->
+                        Unboxed.empty
+                    | numBasicBlocks < 8 ->
+                        Unboxed.generate span (\i ->
+                                 if  i < 2
+                            then let w16 j = count (basicBlockBegin + 4 * i + j)
+                                           - count basicBlockBegin
+                                     w64   =                    w16 0
+                                         .|. Bits.unsafeShiftL (w16 1) 16
+                                         .|. Bits.unsafeShiftL (w16 2) 32
+                                         .|. Bits.unsafeShiftL (w16 3) 48
+                                 in  w64
+                             else 0 )
+                    | numBasicBlocks < 64 ->
+                        Unboxed.generate span (\i ->
+                                 if  i < 2
+                            then let w16 j = count (basicBlockBegin + 8 * (4 * i + j))
+                                           - count basicBlockBegin
+                                     w64   =                    w16 0
+                                         .|. Bits.unsafeShiftL (w16 1) 16
+                                         .|. Bits.unsafeShiftL (w16 2) 32
+                                         .|. Bits.unsafeShiftL (w16 3) 48
+                                 in  w64
+                            else if  i < 18
+                            then let w16 j = count (basicBlockBegin + 4 * (i - 2) + j)
+                                           - count basicBlockBegin
+                                     w64   =                    w16 0
+                                         .|. Bits.unsafeShiftL (w16 1) 16
+                                         .|. Bits.unsafeShiftL (w16 2) 32
+                                         .|. Bits.unsafeShiftL (w16 3) 48
+                                 in  w64
+                            else 0 )
+                    | numBasicBlocks < 128 ->
+                        let ones =
+                                oneIndices (Primitive.unsafeSlice p (q - p) v)
+                        in  Unboxed.generate span (\i ->
+                                let w16 j = fromIntegral (locate ones (4 * i + j))
+                                    w64 =                      w16 0
+                                        .|. Bits.unsafeShiftL (w16 1) 16
+                                        .|. Bits.unsafeShiftL (w16 2) 32
+                                        .|. Bits.unsafeShiftL (w16 3) 48
+                                in  w64 )
+                    | numBasicBlocks < 256 ->
+                        let ones =
+                                oneIndices (Primitive.unsafeSlice p (q - p) v)
+                        in  Unboxed.generate span (\i ->
+                                let w32 j = fromIntegral (locate ones (2 * i + j))
+                                    w64 =                      w32 0
+                                        .|. Bits.unsafeShiftL (w32 1) 32
+                                in  w64 )
+                    | numBasicBlocks < 512 ->
+                        let ones =
+                                oneIndices (Primitive.unsafeSlice p (q - p) v)
+                        in  Unboxed.generate span (\i ->
+                                let w64 = fromIntegral (p + locate ones i)
+                                in  w64 ) )
+        ) (Unboxed.zip v1 (Unboxed.drop 1 v1))
 
 {-| Like `rank` except that the bit index is not checked
 
     This will silently fail and return garbage if you supply an invalid index
 -}
 unsafeRank :: SuccinctBitVector -> Int -> Word64
-unsafeRank (SuccinctBitVector _ rank9_ bits_) p =
+unsafeRank (SuccinctBitVector _ rank9_ _ bits_) p =
         f
     +   ((Bits.unsafeShiftR s (fromIntegral ((t + (Bits.unsafeShiftR t 60 .&. 0x8)) * 9))) .&. 0x1FF)
     +   popCount (Primitive.unsafeIndex bits_ w .&. mask)
