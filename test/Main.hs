c3edcde185ee158c61e1476d6d4da00ddb7ce762
Gabriel Gonzalez <gabriel@awakenetworks.com>
2016-04-11 17:37:20 -0700

    Update tests to use cs-poppy code

--- test/Main.hs
+++ test/Main.hs
@@ -5,73 +5,22 @@ import Succinct.Vector
 import Test.DocTest (doctest)
 import Test.QuickCheck (Property, Small(..), quickCheck, (==>))
 
-testL :: SuccinctBitVector a => a -> Small Word64 -> Property
-testL sbv (Small w64) =
-        (c0 < fst (partialRank sbv n))
-    ==> (c0 == c1 + c2)
-  where
-    -- The (-1) correction is because several indices only support `rank` for
-    -- `Position`s less than the index `size`
-    n = max 0 (size sbv - 1)
+import qualified Data.Vector.Primitive as Primitive
 
-    c0 = Count w64
+testL :: SuccinctBitVector -> Small Int -> Property
+testL sbv (Small n) =
+        0 <= n
+    ==> n <  Primitive.length (vector sbv) * 64
+    ==> n <= unsafeSelect sbv (unsafeRank sbv n)
 
-    (p1, c1) = partialSelect sbv c0
-    (c2, _ ) = partialRank   sbv p1
-
-testR :: SuccinctBitVector a => a -> Small Int -> Property
-testR sbv (Small i) =
-        (0 <= p0 && p0 <= size sbv)
-    ==> (c1 < fst (partialRank sbv n))
-    ==> (p0 <= p1 + p2)
-  where
-    n = max 0 (size sbv - 1)
-
-    p0 = Position i
-
-    (c1, p1) = partialRank   sbv p0
-    (p2, _ ) = partialSelect sbv c1
-
-word64L :: Word64 -> Small Word64 -> Property
-word64L = testL
-
-word64R :: Word64 -> Small Int -> Property
-word64R = testR
-
-basicBlockL :: BasicBlock -> Small Word64 -> Property
-basicBlockL = testL
-
-basicBlockR :: BasicBlock -> Small Int -> Property
-basicBlockR = testR
-
-intermediateBlockL :: IntermediateBlock -> Small Word64 -> Property
-intermediateBlockL = testL
-
-intermediateBlockR :: IntermediateBlock -> Small Int -> Property
-intermediateBlockR = testR
-
-superBlockL :: IntermediateBlock -> Small Word64 -> Property
-superBlockL = testL
-
-superBlockR :: IntermediateBlock -> Small Int -> Property
-superBlockR = testR
-
-bitVectorL :: BitVector -> Small Word64 -> Property
-bitVectorL = testL
-
-bitVectorR :: BitVector -> Small Int -> Property
-bitVectorR = testR
+testR :: SuccinctBitVector -> Small Word64 -> Property
+testR sbv (Small w64) =
+        0   <= w64
+    ==> w64 <  numOnes sbv
+    ==> w64 == unsafeRank sbv (unsafeSelect sbv w64)
 
 main :: IO ()
 main = do
-    quickCheck word64L
-    quickCheck word64R
-    quickCheck basicBlockL
-    quickCheck basicBlockR
-    quickCheck intermediateBlockL
-    quickCheck intermediateBlockR
-    quickCheck superBlockL
-    quickCheck superBlockR
-    quickCheck bitVectorL
-    quickCheck bitVectorR
+    quickCheck testL
+    quickCheck testR
     doctest ["src/Succinct/Vector.hs"]
